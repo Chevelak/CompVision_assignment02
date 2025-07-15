086c984c4adda06a06d28815c46b1edd0393ba8c
Tomas <vlacht@centrum.cz>
2025-07-15 21:16:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,15 +44,29 @@ plt.imshow(lab_my)
 plt.title("Vypočítaná hodnota luminance")
 plt.axis("off")
 
+# %%
+# Výpočet převodu do černobílé podle vzorců z prezentace
+# Převod z sRGB
+img2 = img.astype(np.float32)/255
+img_line = np.where(img2 <= 0.04045, img2 / 12.92, ((img2 + 0.055) / 1.055) ** 2.4)
+
+lumi = 0.2127 * img_line[:,:,2] + 0.7152 * img_line[:,:,1] + 0.0722 * img_line[:,:,0]
+
+# Převod zpět do sRGB
+srgb = (np.where(lumi <= 0.0031308, lumi * 12.92, 1.055 * (lumi ** (1/2.4)) - 0.055))*255
+
+gray = (srgb).astype(np.uint8)
+plt.imshow(gray, 'gray')
+
 
 # %%
 # Z luminiscence do černobílé
-img2 = cv2.imread(img_path, cv2.IMREAD_COLOR)
-gray_cv = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+img3 = cv2.imread(img_path, cv2.IMREAD_COLOR)
+gray_cv = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
 gray_sk = (
-    img2[..., 0].astype(np.float32) * 0.11 +
-    img2[..., 1].astype(np.float32) * 0.59 +
-    img2[..., 2].astype(np.float32) * 0.30
+    img3[..., 0].astype(np.float32) * 0.11 +
+    img3[..., 1].astype(np.float32) * 0.59 +
+    img3[..., 2].astype(np.float32) * 0.30
 ).round().clip(0,255).astype(np.uint8)
 
 
@@ -66,6 +80,7 @@ plt.subplot(1, 2, 2)
 plt.imshow(gray_sk, 'gray')
 plt.title("Černobílá z skimage")
 plt.axis("off")
+
 
 
 
