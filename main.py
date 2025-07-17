3ff36aa5371ba45f5583402dfeff26e994d4bddc
Tomas <vlacht@centrum.cz>
2025-07-17 15:55:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,19 +23,24 @@ lumi = 0.2127 * img_line[:,:,2] + 0.7152 * img_line[:,:,1] + 0.0722 * img_line[:
 # %%
 # Převod zpět do sRGB
 srgb = np.where(lumi <= 0.0031308, lumi * 12.92, 1.055 * np.power(lumi, 1/2.4) - 0.055) * 255
-
 gray = (srgb).astype(np.uint8)
 
 # %%
-# Přepočet z relativní luminance 
-b, g, r = img[...,0], img[...,1], img[...,2]
-gray_match = (0.11 * b + 0.59 * g + 0.3 * r).round().clip(0,255).astype(np.uint8)
-
-plt.imshow(gray_match, 'gray')
+plt.imshow(gray, 'gray')
 plt.title("Černobílá vypočítaná")
+
+
+
 
 # %%
 # OpenCV černobílá
 gray_cv = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray_cv, 'gray')
 plt.title("Černobílá z OpenCV")
+
+# %%
+# Z RGB do černobílé
+b, g, r = img[...,0], img[...,1], img[...,2]
+gray_match = (0.11 * b + 0.59 * g + 0.3 * r).round().clip(0,255).astype(np.uint8)
+plt.imshow(gray_match, 'gray')
+plt.title("Černobílá")
